bef2b14f5c9d64890cddd7d813518cea7b83935e
chanlog610 <dev@ayamichanlog.xyz>
2016-03-22 00:34:16 +0900

    簡易setup追加

--- README.md
+++ README.md
@@ -24,44 +24,4 @@ Javascriptに比べて得られる情報は少ないですが
 [簡易ビューア](http://ayamichanlog.xyz/dev/logcheck.php)
 
 ### 設置方法
-`access_init.php`を編集し以下の項目を任意で変更します。
-* $userdir
-
-ログを出力するディレクトリです  
-あらかじめ作成しておく必要があります    
-
-初期設定では  
-`$userdir = $_SERVER["DOCUMENT_ROOT"] . "/log/";`   
-となってます
-  
-よくわからない方は  
-`<?php echo $_SERVER["DOCUMENT_ROOT"]; ?>` 
-を適当なコードに挿入し、DOCUMENT ROOTと  
-出力したいディレクトリの位置関係を確認してください  
-
-* $file_name
-
-ログの出力を「月ごと」or「日ごと」に切り替えることができます  
-初期設定では  
-`$file_name = date('Ymd', $_SERVER["REQUEST_TIME"]);`  
-となっており「日ごと」ですが`'Ym'`に変更すると「月ごと」のログを出力できます。　　
-
-* $ip_or_host
-
-.logファイルにIPかHostNameどちらを記録するか選べます  
-IPなら0、ホスト名なら1を選択してください  
-簡易ビューアのほうには両方記録しています  
-
-* $EXCLUDE_IP
-* $EXCLUDE_HOST
-* $EXCLUDE_USER_AGENT 
-
-除外するIP/HOST/User Agentを設定します  
-正規表現が使えます　　　　
-
-設定が終わったら
-`access.php`を各ページで読み込みます。  
-例:DOCUMENT　ROOT上に`access.php`がある場合  
-`<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/access.php'; ?>`  
-となります  
-絶対パスなので使いまわしができ、どの階層からも読み込むこめるため管理が楽です
+サーバーにアップロードし、`access_setup.php`にアクセスし各項目を変更します。
---
+++ access_setup.php
@@ -0,0 +1,175 @@
+<?php
+// 設定ファイル読み込み
+require_once "access_init.php";
+
+// 除外リストの配列を;で連結
+$EXCLUDE_IP_implode         = implode(";", $EXCLUDE_IP);
+$EXCLUDE_HOST_implode       = implode(";", $EXCLUDE_HOST);
+$EXCLUDE_USER_AGENT_implode = implode(";", $EXCLUDE_USER_AGENT);
+
+// ディレクトリを検出
+$uri           = $_SERVER["REQUEST_URI"];
+$uridir        = preg_replace("/(.*)access_setup.php/i", "$1", $uri);
+$target_logdir = "'" . $uridir . "log/'";
+$target_php    = "'" . $uridir . "access.php'";
+
+// ファイル名の日付判定
+$check_day_or_month = "";
+if (strlen($file_name) == 6) {
+    $check_day_or_month = 0;
+} elseif (strlen($file_name) == 8) {
+    $check_day_or_month = 1;
+}
+
+// checkedを入れるか判定
+$checked_daily = "";
+if ($check_day_or_month == "1") {
+    $checked_daily = "checked";
+}
+$checked_monthly = "";
+if ($check_day_or_month == "0") {
+    $checked_monthly = "checked";
+}
+$checked_ip = "";
+if ($ip_or_host == 0) {
+    $checked_ip = "checked";
+}
+$checked_host = "";
+if ($ip_or_host == 1) {
+    $checked_host = "checked";
+}
+
+
+// POSTを取得したら実行
+if (isset($_POST['send'])) {
+
+// 設定ファイル読み込み
+    $open_init = file_get_contents("access_init.php");
+// 改行で配列に
+    $init_array = explode("\n", $open_init);
+
+// データ取得
+    $post_logdir                = $_POST['logdir'];
+    $post_day_or_month          = $_POST['day_or_month'];
+    $post_ip_or_host            = $_POST['ip_or_host'];
+    $post_EXCLUDE_IP            = $_POST['EXCLUDE_IP'];
+    $replace_EXCLUDE_IP         = '("' . preg_replace("/;/", "\",\"", $post_EXCLUDE_IP) . '")';
+    $post_EXCLUDE_HOST          = $_POST['EXCLUDE_HOST'];
+    $replace_EXCLUDE_HOST       = '("' . preg_replace("/;/", "\",\"", $post_EXCLUDE_HOST) . '")';
+    $post_EXCLUDE_USER_AGENT    = $_POST['EXCLUDE_USER_AGENT'];
+    $replace_EXCLUDE_USER_AGENT = '("' . preg_replace("/;/", "\",\"", $post_EXCLUDE_USER_AGENT) . '")';
+
+    $init_output = "";
+// 配列をforeachでぶん回して置換
+    foreach ($init_array as $init_key => $init_value) {
+        if (preg_match('/^\$userdir = .*;/', $init_value)) {
+            $init_output .= preg_replace('/(^\$userdir = )(.*);/', "$1$post_logdir;", $init_value) . "\n";
+        } elseif (preg_match('/^\$file_name = .*;/', $init_value)) {
+            $init_output .= preg_replace('/(Ymd|Ym)/', "$post_day_or_month", $init_value) . "\n";
+        } elseif (preg_match('/^\$ip_or_host = .*;/', $init_value)) {
+            $init_output .= preg_replace('/(0|1)/', "$post_ip_or_host", $init_value) . "\n";
+        } elseif (preg_match('/^\$EXCLUDE_IP = .*;/', $init_value)) {
+            $init_output .= preg_replace('/(^\$EXCLUDE_IP = array).*;/', "$1$replace_EXCLUDE_IP;", $init_value) . "\n";
+        } elseif (preg_match('/^\$EXCLUDE_HOST = .*;/', $init_value)) {
+            $init_output .= preg_replace('/(^\$EXCLUDE_HOST = array).*;/', "$1$replace_EXCLUDE_HOST;", $init_value) . "\n";
+        } elseif (preg_match('/^\$EXCLUDE_USER_AGENT = .*;/', $init_value)) {
+            $init_output .= preg_replace('/(^\$EXCLUDE_USER_AGENT = array).*;/', "$1$replace_EXCLUDE_USER_AGENT;", $init_value) . "\n";
+        } else {
+            $init_output .= $init_value . "\n";
+        }
+    }
+// 配列を改行し書き込む
+    file_put_contents("access_init.php", $init_output);
+}
+?>
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta charset="utf-8">
+<title>設定</title>
+<style type="text/css">
+body {
+  line-height: 1.5;
+  font-size: 93%;
+}
+table {
+  border-collapse: collapse;
+}
+td {
+  padding: 5px;
+  border: solid 1px #aaa;
+}
+</style>
+</head>
+
+<body>
+  <h1>設定</h1>
+  <hr style="margin: 20px 0"> 各ページに以下のコードを挿入してください
+  <br>
+  <span style="border: solid 1px #ccc;margin: 10px;padding: 5px;">
+&lt;?php require_once $_SERVER['DOCUMENT_ROOT'].<?=$target_php?>;
+?&gt;
+</span>
+  <form method="post" id="form" action="" style="margin-top: 20px;">
+    <table>
+      <tr>
+        <td>logファイル出力先のディレクトリ
+          <br>(先に作成してください)</td>
+        <td style="font-size:90%;">
+          <input type="text" name="logdir" value="$_SERVER['DOCUMENT_ROOT'].<?=$target_logdir?>" style="width: 500px">
+          <br>参考:access_setup.phpは<b>$_SERVER['DOCUMENT_ROOT'].'<?=$uridir?>'</b>内に設置されいます
+        </td>
+      </tr>
+      <tr>
+        <td>logファイルの出力設定
+          <br>(月ごとまたは日ごと)</td>
+        <td>
+          <input type="radio" name="day_or_month" value="Ymd" <?=$checked_daily?>>daily
+          <br>
+          <input type="radio" name="day_or_month" value="Ym" <?=$checked_monthly?>>monthly
+        </td>
+      </tr>
+      <tr>
+        <td>logファイルのホスト記録設定
+          <br>(IPまたはHostName)</td>
+        <td>
+          <input type="radio" name="ip_or_host" value="0" <?=$checked_ip?>>IP
+          <br>
+          <input type="radio" name="ip_or_host" value="1" <?=$checked_host?>>HostName
+        </td>
+      </tr>
+      <tr>
+        <td>IP除外リスト
+          <br>(正規表現 <b>;</b>区切り)</td>
+        <td>
+          <input type="text" name="EXCLUDE_IP" value="<?=$EXCLUDE_IP_implode?>" style="width: 500px">
+        </td>
+      </tr>
+      <tr>
+        <td>HostName除外リスト
+          <br>(正規表現 <b>;</b>区切り)</td>
+        <td>
+          <input type="text" name="EXCLUDE_HOST" value="<?=$EXCLUDE_HOST_implode?>" style="width: 500px">
+        </td>
+      </tr>
+      <tr>
+        <td>UserAgent除外リスト
+          <br>(正規表現 <b>;</b>区切り)</td>
+        <td>
+          <input type="text" name="EXCLUDE_USER_AGENT" value="<?=$EXCLUDE_USER_AGENT_implode?>" style="width: 500px">
+        </td>
+      </tr>
+    </table>
+    <input type="hidden" name="send" value="send">
+    <div style="text-align: center;margin: 10px auto;">
+      <input type="submit" value="保存">
+      <a href="javascript:location.reload(true);" style="text-decoration: none;">
+        <input type="button" value="結果を反映させる">
+      </a>
+    </div>
+  </form>
+</body>
+
+</html>
